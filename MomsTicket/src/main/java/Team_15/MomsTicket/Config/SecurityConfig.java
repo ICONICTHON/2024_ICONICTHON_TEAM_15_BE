31de8a1aa485a1dedbeb50f7cfebef5eb7c46bd4
anismynm <ddoe0318@naver.com>
2024-11-15 05:50:06 +0900

    Update SecurityConfig.java

--- MomsTicket/src/main/java/Team_15/MomsTicket/Config/SecurityConfig.java
+++ MomsTicket/src/main/java/Team_15/MomsTicket/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -11,7 +12,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+
                         .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 );
